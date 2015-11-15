ece8916e4ce3a272a644254a844f3d49ca3b4c3b
jacgrady1 <lgxjackyzyx@gmail.com>
2015-11-15 14:49:57 -0800

    website framework built, data unfinished yet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+// var fs = require('fs');
+// fs.readFile('./data.json', function(err,data){
+//     if(err) {
+//         throw err;
+//     }
+//     console.log(data);
+//
+// })
+
+var data = require('../data/data.json');
+var courses = data.courses;
+var feedbacks = data.feedbacks;
+var lecturers = data.lecturers;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        courses: courses,
+        feedbacks: feedbacks,
+        lecturers: lecturers,
+        title: "TFT"});
+});
+
+router.get('/course/:id*', function(req, res, next) {
+    var id = req.params.id;
+    var course = courses[0];
+    var arrayLength = courses.length;
+    for (var i = 0; i < arrayLength; i++) {
+        if (id==courses[i].id){
+            course = courses[i];
+        }
+    }
+    res.render('course', {
+        course: course,
+        title: "course"
+    });
 });
 
 module.exports = router;
