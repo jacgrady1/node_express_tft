a714aced9ff0815d851c925b5eaa390071e7cf9f
jacgrady1 <lgxjackyzyx@gmail.com>
2015-12-04 13:15:20 -0800

    fix flightplan and bundle min

--- flightplan.js
+++ flightplan.js
@@ -51,6 +51,10 @@ plan.remote(function(remote) {
   remote.log('Install dependencies');
   remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
 
+  remote.log('Build dev dependencies');
+  remote.sudo('npm --production --prefix ~/' + tmpDir + ' build ~/' + tmpDir, {user: username});
+
+
   remote.log('Reload application');
   remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
   remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
